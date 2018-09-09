5b69dbc67fb28584eb8eb7da52ad016f04008b18
cmestradap <carlos1116@gmail.com>
2018-09-09 02:05:02 -0500

    Proyecto Base con las vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/user.model.ts
@@ -0,0 +1,8 @@
+export class User {
+    Cedula: number;
+    Password: string;
+    Email: string; //recordar la validacion del @unal.edu.co
+    FirtsName: string;
+    LastName: string;
+    
+}
